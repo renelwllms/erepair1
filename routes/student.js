ec43004acfa2913e246e654a3b26537937d0d81b
jasminyu112700 <sunshine.1127@icloud.com>
2024-09-01 10:06:47 +0800

    feat: assigned & course-student

--- routes/student.js
+++ routes/student.js
@@ -7,27 +7,34 @@ const sql = require("mssql");
 router.get("/list", isAuthenticated, async function (req, res, next) {
   try {
     const request = new sql.Request();
-
+    if (req?.info?.givenName !== "Admin") {
+      return res.send({ code: 403, data: [], message: "no permission" });
+    }
     const current = Number(req.query.current || 1);
     const pageSize = Number(req.query.pageSize || 10);
     const startIndex = (current - 1) * pageSize;
     const name = req.query.StudentName || "";
+    const SchoolName = req.query.SchoolName || "";
 
     request.input("name", sql.VarChar, name);
+    request.input("SchoolName", sql.VarChar, SchoolName);
     request.input("startIndex", sql.Int, startIndex);
     request.input("pageSize", sql.Int, pageSize);
 
     const query = `
-    SELECT COUNT(*) AS totalRows 
-    FROM tblStudent 
-    WHERE StudentName LIKE '%' + @name + '%';
-    WITH s AS (
-      SELECT * FROM tblStudent WHERE StudentName LIKE '%' + @name + '%'
-    )
     SELECT s.*, sw.SchoolName, sw.Email
-    FROM  s
+    INTO #TempStudent
+    FROM tblStudent s
     LEFT OUTER JOIN tblSchoolWorkplace sw ON s.SchoolNumber = sw.SchoolNumber
-    ORDER BY StudentID OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY`;
+    WHERE s.StudentName LIKE '%' + @name + '%' 
+    AND sw.SchoolName LIKE '%' + @SchoolName + '%'
+
+    SELECT COUNT(*) AS totalRows FROM #TempStudent;
+    SELECT * FROM #TempStudent
+    ORDER BY StudentID 
+    OFFSET @startIndex ROWS
+    FETCH NEXT @pageSize ROWS ONLY;
+    DROP TABLE #TempStudent;`;
 
     request.query(query, (err, result) => {
       if (err) console.log(err);
@@ -81,6 +88,38 @@ router.get("/course", isAuthenticated, async function (req, res, next) {
           data: currentPageData || [],
         });
       }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/assigned", isAuthenticated, async function (req, res, next) {
+  try {
+    if (req?.info?.givenName !== "Admin") {
+      return res.send({ code: 403, message: "no permission" });
+    }
+    const request = new sql.Request();
+    console.log(req.body);
+    const id = Number(req.body.id);
+    const tutor = req.body.tutor || "";
+    if (!id || !tutor) {
+      return res.send({ code: 1, message: "Please select" });
+    }
+    request.input("id", sql.Int, id);
+    request.input("Tutor", sql.VarChar, tutor);
+    const query = `
+    UPDATE tblStudent
+    SET Tutor = @Tutor
+    WHERE StudentID = @id;
+    `;
+
+    request.query(query, (err) => {
+      if (err) console.log(err);
+      return res.send({
+        code: 0,
+        data: "success",
+      });
     });
   } catch (error) {
     next(error);
--- routes/tutor.js
+++ routes/tutor.js
@@ -27,4 +27,108 @@ router.get("/list", isAuthenticated, async function (req, res, next) {
   }
 });
 
+router.get("/student", isAuthenticated, async function (req, res, next) {
+  try {
+    const request = new sql.Request();
+
+    const current = Number(req.query.current || 1);
+    const pageSize = Number(req.query.pageSize || 10);
+    const startIndex = (current - 1) * pageSize;
+    const name = req.query.StudentName || "";
+    const SchoolName = req.query.SchoolName || "";
+
+    request.input("name", sql.VarChar, name);
+    request.input("SchoolName", sql.VarChar, SchoolName);
+    request.input("startIndex", sql.Int, startIndex);
+    request.input("pageSize", sql.Int, pageSize);
+    if (req?.info?.givenName !== "admin") {
+      request.input(
+        "DeliverySpecialist",
+        sql.VarChar,
+        req?.info?.displayName
+      );
+    }
+
+    const query = `
+    SELECT s.*, sw.SchoolName, sw.Email
+    INTO #TempStudent
+    FROM tblStudent s
+    LEFT OUTER JOIN tblSchoolWorkplace sw ON s.SchoolNumber = sw.SchoolNumber
+    WHERE s.StudentName LIKE '%' + @name + '%' 
+    AND sw.SchoolName LIKE '%' + @SchoolName + '%'
+
+    SELECT COUNT(*) AS totalRows FROM #TempStudent;
+    SELECT * FROM #TempStudent
+    ORDER BY StudentID 
+    OFFSET @startIndex ROWS
+    FETCH NEXT @pageSize ROWS ONLY;
+    DROP TABLE #TempStudent;`;
+
+    request.query(query, (err, result) => {
+      if (err) console.log(err);
+      if (result?.recordset) {
+        const total = result.recordsets[0][0].totalRows;
+        const currentPageData = result.recordsets[1];
+
+        return res.send({
+          code: 0,
+          data: currentPageData,
+          pagination: getPagination(current, pageSize, total),
+        });
+      }
+
+      return res.send({ code: 0, data: [], pagination: getPagination() });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/course/student", isAuthenticated, async function (req, res, next) {
+  try {
+    const request = new sql.Request();
+    const CourseID = Number(req.query.CourseID);
+    const UnitStandardID = Number(req.query.UnitStandardID);
+    const DeliverySpecialist = req.query.DeliverySpecialist || "";
+    const CourseDate = req.query.CourseDate || "";
+    if (!CourseID || !UnitStandardID || !DeliverySpecialist) {
+      return res.send({ code: 0, data: [] });
+    }
+    request.input("CourseID", sql.Int, CourseID);
+    request.input("UnitStandardID", sql.Int, UnitStandardID);
+    request.input("DeliverySpecialist", sql.VarChar, DeliverySpecialist);
+    request.input("CourseDate", sql.VarChar, CourseDate);
+    const query = `
+    WITH CTE AS (
+      SELECT DISTINCT sc.StudentID,sc.CourseDate
+      FROM tblStudentCourse sc
+      RIGHT OUTER JOIN tblStudentCourseUnitStandard scu ON scu.StudentCourseID = sc.StudentCourseID
+      WHERE sc.DeliverySpecialist = @DeliverySpecialist
+        AND sc.CourseID = @CourseID
+        AND sc.CourseDate LIKE '%' + @CourseDate + '%' 
+        AND scu.UnitStandardID = @UnitStandardID
+   
+
+  )
+  SELECT s.*, cte.CourseDate
+  FROM CTE cte
+  LEFT JOIN tblStudent s ON cte.StudentID = s.StudentID;`;
+
+    request.query(query, (err, result) => {
+      if (err) console.log(err);
+      if (result?.recordset) {
+        const d = result.recordsets[0];
+
+        return res.send({
+          code: 0,
+          data: d,
+        });
+      }
+      return res.send({ code: 0, data: [] });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
