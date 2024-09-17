74fd669184d8c872f4b620b6698765b29d889e11
jasminyu112700 <sunshine.1127@icloud.com>
2024-09-17 08:33:42 +0800

    feat: My Workshops

--- routes/course.js
+++ routes/course.js
@@ -7,20 +7,53 @@ const sql = require("mssql");
 router.get("/list", isAuthenticated, async function (req, res, next) {
   try {
     const pool = await getPool();
-    const request = await pool.request()
+    const request = await pool.request();
 
-    const query = `SELECT * FROM tblCourse`;
+    let query = `
+      SELECT c.*, 
+        (SELECT STRING_AGG(CAST(ucs.UnitStandardID AS VARCHAR), ',') 
+         FROM tblCourseUnitStandard ucs 
+         WHERE ucs.CourseID = c.CourseID) AS UnitStandardIDs
+      FROM tblCourse c
+      WHERE 1=1`;
+
+    const CourseName = req.query.CourseName;
+    const CourseLevel = Number(req.query.CourseLevel);
+    const CourseCredits = Number(req.query.CourseCredits);
+
+    if (CourseName) {
+      request.input("CourseName", sql.VarChar, CourseName);
+      query += ` AND [CourseName] LIKE '%' + @CourseName + '%'`;
+    }
+    if (CourseLevel) {
+      request.input("CourseLevel", sql.Int, CourseLevel);
+      query += ` AND CourseLevel = @CourseLevel`;
+    }
+    if (CourseCredits) {
+      request.input("CourseCredits", sql.Int, CourseCredits);
+      query += ` AND CourseCredits = @CourseCredits`;
+    }
 
     request.query(query, (err, result) => {
-      if (err) console.log(err);
+      if (err) {
+        console.log(err);
+        return res.send({ code: 1, message: "Error occurred" });
+      }
+
       if (result?.recordset) {
-        const d = result.recordsets[0];
+        const data = result.recordset.map((course) => ({
+          ...course,
+          UnitStandardIDs: course.UnitStandardIDs
+            ? course.UnitStandardIDs.split(",").map((id) => parseInt(id))
+            : [],
+        }));
 
         return res.send({
           code: 0,
-          data: d,
+          data: data,
         });
       }
+
       return res.send({ code: 0, data: [] });
     });
   } catch (error) {
@@ -31,7 +64,7 @@ router.get("/list", isAuthenticated, async function (req, res, next) {
 router.get("/unit", isAuthenticated, async function (req, res, next) {
   try {
     const pool = await getPool();
-    const request = await pool.request()
+    const request = await pool.request();
     const id = Number(req.query.id);
 
     if (!id) {
@@ -55,6 +88,253 @@ router.get("/unit", isAuthenticated, async function (req, res, next) {
         });
       }
       return res.send({ code: 0, data: [] });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/units", isAuthenticated, async function (req, res, next) {
+  try {
+    const pool = await getPool();
+    const request = await pool.request();
+
+    let query = `SELECT * FROM tblUnitStandard WHERE 1=1`;
+
+    const name = req.query.name;
+    const USLevel = Number(req.query.USLevel);
+    const USCredits = Number(req.query.USCredits);
+
+    if (name) {
+      request.input("name", sql.VarChar, name);
+      query += `AND ([USName] LIKE '%' + @name + '%' OR [US] LIKE '%' + @name + '%')`;
+    }
+
+    if (USLevel) {
+      request.input("USLevel", sql.Int, USLevel);
+      query += ` AND USLevel = @USLevel`;
+    }
+    if (USCredits) {
+      request.input("USCredits", sql.Int, USCredits);
+      query += ` AND USCredits = @USCredits`;
+    }
+
+    request.query(query, (err, result) => {
+      if (err) console.log(err);
+      if (result?.recordset) {
+        const d = result.recordsets[0];
+
+        return res.send({
+          code: 0,
+          data: d,
+        });
+      }
+      return res.send({ code: 0, data: [] });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", isAuthenticated, async (req, res, next) => {
+  const {
+    CourseName,
+    CourseDetails,
+    CourseLevel,
+    CourseCredits,
+    CourseDelivery,
+    CourseGroup,
+    UnitStandardIDs,
+  } = req.body;
+  const pool = await getPool();
+  const transaction = new sql.Transaction(pool);
+  try {
+    await transaction.begin();
+
+    const request = new sql.Request(transaction);
+
+    // Insert into tblCourse
+    let courseQuery = `
+      INSERT INTO tblCourse (CourseName, CourseDetails, CourseLevel, CourseCredits, CourseDelivery, CourseGroup, CreateDate, CreateUser, UpdateDate, UpdateUser)
+      VALUES (@CourseName, @CourseDetails, @CourseLevel, @CourseCredits, @CourseDelivery, @CourseGroup, GETDATE(), @CreateUser, GETDATE(), @CreateUser);
+    `;
+    request.input("CourseName", sql.VarChar, CourseName);
+    request.input("CourseDetails", sql.Text, CourseDetails);
+    request.input("CourseLevel", sql.Int, CourseLevel);
+    request.input("CourseCredits", sql.Int, CourseCredits);
+    request.input("CourseDelivery", sql.VarChar, CourseDelivery);
+    request.input("CourseGroup", sql.VarChar, CourseGroup);
+    request.input("CreateUser", sql.VarChar, req?.info?.displayName);
+
+    await request.query(courseQuery);
+    const courseIdResult = await request.query(
+      "SELECT SCOPE_IDENTITY() AS CourseID"
+    );
+    const CourseID = courseIdResult.recordset[0].CourseID;
+
+    // Insert into tblCourseUnitStandard
+    if (UnitStandardIDs && UnitStandardIDs.length > 0) {
+      for (const UnitStandardID of UnitStandardIDs) {
+        let unitStandardQuery = `
+          INSERT INTO tblCourseUnitStandard (CourseID, UnitStandardID)
+          VALUES (@CourseID, @UnitStandardID);
+        `;
+        request.input("CourseID", sql.Int, CourseID);
+        request.input("UnitStandardID", sql.Int, UnitStandardID);
+        await request.query(unitStandardQuery);
+      }
+    }
+
+    await transaction.commit();
+    res.send({ code: 0, data: true, message: "Course created successfully" });
+  } catch (error) {
+    await transaction.rollback();
+    next(error);
+  }
+});
+
+router.put("/", isAuthenticated, async (req, res, next) => {
+  const {
+    CourseID,
+    CourseName,
+    CourseDetails,
+    CourseLevel,
+    CourseCredits,
+    CourseDelivery,
+    CourseGroup,
+    UnitStandardIDs,
+  } = req.body;
+  const pool = await getPool();
+  const transaction = new sql.Transaction(pool);
+  try {
+    await transaction.begin();
+
+    const request = new sql.Request(transaction);
+
+    // Update tblCourse
+    let updateCourseQuery = `
+      UPDATE tblCourse
+      SET CourseName = @CourseName, CourseDetails = @CourseDetails, CourseLevel = @CourseLevel,
+          CourseCredits = @CourseCredits, CourseDelivery = @CourseDelivery, CourseGroup = @CourseGroup,
+          UpdateDate = GETDATE(), UpdateUser = @UpdateUser
+      WHERE CourseID = @CourseID;
+    `;
+    request.input("CourseID", sql.Int, CourseID);
+    request.input("CourseName", sql.VarChar, CourseName);
+    request.input("CourseDetails", sql.Text, CourseDetails);
+    request.input("CourseLevel", sql.Int, CourseLevel);
+    request.input("CourseCredits", sql.Int, CourseCredits);
+    request.input("CourseDelivery", sql.VarChar, CourseDelivery);
+    request.input("CourseGroup", sql.VarChar, CourseGroup);
+    request.input("UpdateUser", sql.VarChar, req?.info?.displayName);
+    await request.query(updateCourseQuery);
+
+    // Delete existing Unit Standards
+    let deleteUnitsQuery = `DELETE FROM tblCourseUnitStandard WHERE CourseID = @CourseID;`;
+    await request.query(deleteUnitsQuery);
+
+    // Insert new Unit Standards
+    if (UnitStandardIDs && UnitStandardIDs.length > 0) {
+      let insertUnitsQuery = `
+        INSERT INTO tblCourseUnitStandard (CourseID, UnitStandardID) 
+        VALUES 
+        ${UnitStandardIDs.map(
+          (_, idx) => `(@CourseID, @UnitStandardID${idx})`
+        ).join(", ")};
+      `;
+      UnitStandardIDs.forEach((UnitStandardID, idx) => {
+        request.input(`UnitStandardID${idx}`, sql.Int, UnitStandardID);
+      });
+
+      await request.query(insertUnitsQuery);
+    }
+
+    await transaction.commit();
+    res.send({ code: 0, data: "true", message: "Course updated successfully" });
+  } catch (error) {
+    await transaction.rollback();
+    next(error);
+  }
+});
+
+router.delete("/", isAuthenticated, async (req, res, next) => {
+  const { CourseID } = req.body;
+  const pool = await getPool();
+  const transaction = new sql.Transaction(pool);
+  try {
+    await transaction.begin();
+
+    const request = new sql.Request(transaction);
+
+    // Delete from tblCourseUnitStandard
+    let deleteUnitsQuery = `DELETE FROM tblCourseUnitStandard WHERE CourseID = @CourseID;`;
+    request.input("CourseID", sql.Int, CourseID);
+    await request.query(deleteUnitsQuery);
+
+    // Delete from tblCourse
+    let deleteCourseQuery = `DELETE FROM tblCourse WHERE CourseID = @CourseID;`;
+    await request.query(deleteCourseQuery);
+
+    await transaction.commit();
+    res.send({ code: 0, data: "true", message: "Course deleted successfully" });
+  } catch (error) {
+    await transaction.rollback();
+    next(error);
+  }
+});
+
+router.post("/unitstandard", isAuthenticated, async (req, res, next) => {
+  const {
+    UnitStandardID,
+    US,
+    USName,
+    USLevel,
+    USCredits,
+    USDescription,
+    USClassification,
+    USAvailableGrade,
+    USURL,
+    CreateUser,
+  } = req.body;
+
+  try {
+    const pool = await getPool();
+    const request = pool.request();
+
+    if (UnitStandardID) {
+      // Update existing Unit Standard
+      const updateQuery = `
+        UPDATE tblUnitStandard
+        SET US = @US, USName = @USName, USLevel = @USLevel, USCredits = @USCredits, USDescription = @USDescription,
+            USClassification = @USClassification, USAvailableGrade = @USAvailableGrade, USURL = @USURL,
+            UpdateDate = GETDATE(), UpdateUser = @CreateUser
+        WHERE UnitStandardID = @UnitStandardID;
+      `;
+      request.input("UnitStandardID", sql.Int, UnitStandardID);
+    } else {
+      // Insert new Unit Standard
+      const insertQuery = `
+        INSERT INTO tblUnitStandard (US, USName, USLevel, USCredits, USDescription, USClassification, USAvailableGrade, USURL, CreateDate, CreateUser, UpdateDate, UpdateUser)
+        VALUES (@US, @USName, @USLevel, @USCredits, @USDescription, @USClassification, @USAvailableGrade, @USURL, GETDATE(), @CreateUser, GETDATE(), @CreateUser);
+      `;
+    }
+
+    request.input("US", sql.VarChar, US);
+    request.input("USName", sql.VarChar, USName);
+    request.input("USLevel", sql.Int, USLevel);
+    request.input("USCredits", sql.Int, USCredits);
+    request.input("USDescription", sql.Text, USDescription);
+    request.input("USClassification", sql.VarChar, USClassification);
+    request.input("USAvailableGrade", sql.VarChar, USAvailableGrade);
+    request.input("USURL", sql.VarChar, USURL);
+    request.input("CreateUser", sql.VarChar, CreateUser);
+
+    await request.query(UnitStandardID ? updateQuery : insertQuery);
+    res.send({
+      code: 0,
+      message: UnitStandardID
+        ? "Unit Standard updated successfully"
+        : "Unit Standard created successfully",
     });
   } catch (error) {
     next(error);
--- routes/tutor.js
+++ routes/tutor.js
@@ -33,7 +33,7 @@ router.get("/student", isAuthenticated, async function (req, res, next) {
   try {
     const pool = await getPool();
     const request = await pool.request();
-   
+
     const current = Number(req.query.current || 1);
     const pageSize = Number(req.query.pageSize || 10);
     const startIndex = (current - 1) * pageSize;
@@ -90,7 +90,6 @@ router.get("/student", isAuthenticated, async function (req, res, next) {
     next(error);
   }
 });
-
 
 router.get("/course/student", isAuthenticated, async function (req, res, next) {
   try {
@@ -255,46 +254,98 @@ router.get("/verifyCode", async function (req, res, next) {
     const Code = req.query?.Code || "";
     if (!Code) {
       return res.send({
-        code: 0,
+        code: 1,
         data: false,
+        message: "Code is required",
       });
     }
 
     const pool = await getPool();
-    const request = await pool.request();
-    let query = `
-    SELECT CASE 
-    WHEN EXISTS (
-        SELECT 1 FROM tblWorkshop WHERE Code = @Code
-    ) 
-    THEN 1 
-    ELSE 0 
-    END AS DoesExist;
+    const request = new sql.Request(pool);
+
+    // Query to check if Code exists and if the number of students has reached the limit
+    const query = `
+      DECLARE @MaxStudents INT;
+      DECLARE @CurrentCount INT;
+
+      -- Check if Code exists
+      IF EXISTS (SELECT 1 FROM tblWorkshop WHERE Code = @Code)
+      BEGIN
+        -- Get the maximum allowed number of students
+        SELECT @MaxStudents = StudentsNum
+        FROM tblWorkshop
+        WHERE Code = @Code;
+
+        -- Count current number of students
+        SELECT @CurrentCount = COUNT(*)
+        FROM tblWorkshopResult
+        WHERE Code = @Code;
+
+        -- Determine if the number of students has reached the maximum allowed
+        IF (@CurrentCount >= @MaxStudents)
+        BEGIN
+          SELECT 1 AS IsFull; -- Indicates that the workshop is full
+        END
+        ELSE
+        BEGIN
+          SELECT 0 AS IsFull; -- Indicates that there is still space available
+        END
+      END
+      ELSE
+      BEGIN
+        -- Code does not exist
+        SELECT -1 AS IsFull;
+      END
     `;
+
     request.input("Code", sql.VarChar, Code);
 
-    request.query(query, (err, result) => {
-      if (err) console.log(err);
-      if (result?.recordset) {
-        const exists = result.recordset[0].DoesExist;
-        if (exists) {
-          return res.send({
-            code: 0,
-            data: true,
-          });
-        }
+    // Execute query
+    const result = await request.query(query);
+
+    if (result?.recordset) {
+      const isFull = result.recordset[0].IsFull;
+
+      if (isFull === 1) {
+        return res.send({
+          code: 0,
+          data: false,
+          message: "The workshop is full. No more spots available.",
+        });
+      } else if (isFull === -1) {
+        return res.send({
+          code: 0,
+          data: false,
+          message: "Code not found.",
+        });
+      } else {
+        return res.send({
+          code: 0,
+          data: true,
+          message: "The workshop is available.",
+        });
       }
-      return res.send({ code: 0, data: false });
+    }
+
+    // Default response if no recordset found
+    return res.send({
+      code: 0,
+      data: false,
+      message: "Unexpected error occurred.",
     });
   } catch (error) {
+    console.error("Error verifying workshop code:", error);
     next(error);
   }
 });
 
 router.post("/workshop/info", async function (req, res, next) {
+  const pool = await getPool();
+  const transaction = new sql.Transaction(pool); // Initialize transaction
+
   try {
-    const pool = await getPool();
-    const request = await pool.request();
+    await transaction.begin(); // Begin transaction
+    const request = new sql.Request(transaction);
 
     const {
       Code,
@@ -308,14 +359,15 @@ router.post("/workshop/info", async function (req, res, next) {
       Feedback,
     } = req.body;
 
+    // Validate required parameters
     if (!Code) {
-      return res.send({ code: 1, message: "Code Error" });
+      throw new Error("Code is required");
     }
-
     if (!FirstName || !LastName) {
-      return res.send({ code: 1, message: "Please select" });
+      throw new Error("FirstName and LastName are required");
     }
 
+    // Bind parameters
     request.input("Code", sql.VarChar, Code);
     request.input("FirstName", sql.VarChar, FirstName);
     request.input("LastName", sql.VarChar, LastName);
@@ -326,22 +378,353 @@ router.post("/workshop/info", async function (req, res, next) {
     request.input("Ethnicity", sql.VarChar, Ethnicity);
     request.input("Feedback", sql.VarChar, Feedback);
 
+    // Query to perform the operations and get remaining spots
     const query = `
-    DECLARE @CreateDate DATETIME = GETDATE();
-    INSERT INTO tblWorkshopResult (Code,FirstName,LastName,School,Gender,DOB,Email,Ethnicity,Feedback,CreateDate)
-    VALUES (@Code,@FirstName,@LastName,@School,@Gender,@DOB,@Email,@Ethnicity,@Feedback,@CreateDate);
+      DECLARE @CurrentCount INT;
+      DECLARE @MaxStudents INT;
+      DECLARE @RemainingSpots INT;
+
+      -- Lock the tblWorkshopResult table to prevent concurrent inserts
+      SELECT @CurrentCount = COUNT(*)
+      FROM tblWorkshopResult WITH (UPDLOCK, HOLDLOCK)
+      WHERE Code = @Code;
+
+      SELECT @MaxStudents = StudentsNum
+      FROM tblWorkshop 
+      WHERE Code = @Code;
+
+      -- Check if the number of students exceeds the limit
+      IF (@CurrentCount < @MaxStudents)
+      BEGIN
+        -- Insert new student record
+        DECLARE @CreateDate DATETIME = GETDATE();
+        INSERT INTO tblWorkshopResult (
+          Code, FirstName, LastName, School, Gender, DOB, Email, Ethnicity, Feedback, CreateDate
+        ) VALUES (
+          @Code, @FirstName, @LastName, @School, @Gender, @DOB, @Email, @Ethnicity, @Feedback, @CreateDate
+        );
+
+        -- Calculate remaining spots
+        SET @RemainingSpots = @MaxStudents - (@CurrentCount + 1);
+      END
+      ELSE
+      BEGIN
+        -- If the number of students is full, throw an error
+        THROW 50001, 'The workshop has reached its maximum capacity.', 1;
+      END;
+
+      -- Select remaining spots to return in the response
+      SELECT @RemainingSpots AS RemainingSpots;
     `;
 
-    request.query(query, (err) => {
-      if (err) console.log(err);
-      return res.send({
-        code: 0,
-        data: "success",
-      });
+    // Execute the query and insert operation
+    const result = await request.query(query);
+
+    // Commit the transaction
+    await transaction.commit();
+
+    // Return success message with remaining spots
+    return res.send({
+      code: 0,
+      data: true,
+      message: "Student has been successfully added.",
+      remainingSpots: result.recordset[0].RemainingSpots, // Access the remaining spots from the query result
     });
   } catch (error) {
-    next(error);
+    try {
+      // Rollback the transaction if an error occurs
+      await transaction.rollback();
+    } catch (rollbackError) {
+      console.error("Error rolling back transaction:", rollbackError);
+    }
+
+    console.error("Error processing request:", error);
+
+    // Handle and return error messages
+    if (error.number === 50001) {
+      return res.send({
+        code: 0,
+        data: false,
+        message: error.message, // Maximum capacity reached
+      });
+    }
+    next(error); // Handle other unknown errors
   }
 });
+
+router.post("/workshop/info", async function (req, res, next) {
+  const pool = await getPool();
+  const transaction = new sql.Transaction(pool); // Initialize transaction
+
+  try {
+    await transaction.begin(); // Begin transaction
+    const request = new sql.Request(transaction);
+
+    const { Code, FirstName, LastName, School, Gender, DOB, Email, Ethnicity } =
+      req.body;
+
+    // Validate required parameters
+    if (!Code) {
+      throw new Error("Code is required");
+    }
+    if (!FirstName || !LastName) {
+      throw new Error("FirstName and LastName are required");
+    }
+
+    // Bind parameters
+    request.input("Code", sql.VarChar, Code);
+    request.input("FirstName", sql.VarChar, FirstName);
+    request.input("LastName", sql.VarChar, LastName);
+    request.input("School", sql.VarChar, School);
+    request.input("Gender", sql.VarChar, Gender);
+    request.input("DOB", sql.VarChar, DOB);
+    request.input("Email", sql.VarChar, Email);
+    request.input("Ethnicity", sql.VarChar, Ethnicity);
+
+    // Query to perform the operations and get remaining spots
+    const query = `
+      DECLARE @CurrentCount INT;
+      DECLARE @MaxStudents INT;
+      DECLARE @RemainingSpots INT;
+
+      -- Lock the tblWorkshopResult table to prevent concurrent inserts
+      SELECT @CurrentCount = COUNT(*)
+      FROM tblWorkshopResult WITH (UPDLOCK, HOLDLOCK)
+      WHERE Code = @Code;
+
+      SELECT @MaxStudents = StudentsNum
+      FROM tblWorkshop 
+      WHERE Code = @Code;
+
+      -- Check if the number of students exceeds the limit
+      IF (@CurrentCount < @MaxStudents)
+      BEGIN
+        -- Insert new student record
+        DECLARE @CreateDate DATETIME = GETDATE();
+        INSERT INTO tblWorkshopResult (
+          Code, FirstName, LastName, School, Gender, DOB, Email, Ethnicity, Feedback, CreateDate
+        ) VALUES (
+          @Code, @FirstName, @LastName, @School, @Gender, @DOB, @Email, @Ethnicity, @Feedback, @CreateDate
+        );
+
+        -- Calculate remaining spots
+        SET @RemainingSpots = @MaxStudents - (@CurrentCount + 1);
+      END
+      ELSE
+      BEGIN
+        -- If the number of students is full, throw an error
+        THROW 50001, 'The workshop has reached its maximum capacity.', 1;
+      END;
+
+      -- Select remaining spots to return in the response
+      SELECT @RemainingSpots AS RemainingSpots;
+    `;
+
+    // Execute the query and insert operation
+    const result = await request.query(query);
+
+    // Commit the transaction
+    await transaction.commit();
+
+    // Return success message with remaining spots
+    return res.send({
+      code: 0,
+      data: true,
+      message: "Student has been successfully added.",
+      remainingSpots: result.recordset[0].RemainingSpots, // Access the remaining spots from the query result
+    });
+  } catch (error) {
+    try {
+      // Rollback the transaction if an error occurs
+      await transaction.rollback();
+    } catch (rollbackError) {
+      console.error("Error rolling back transaction:", rollbackError);
+    }
+
+    console.error("Error processing request:", error);
+
+    // Handle and return error messages
+    if (error.number === 50001) {
+      return res.send({
+        code: 0,
+        data: false,
+        message: error.message, // Maximum capacity reached
+      });
+    }
+    next(error); // Handle other unknown errors
+  }
+});
+
+router.post(
+  "/workshop/info/extra",
+  isAuthenticated,
+  async function (req, res, next) {
+    try {
+      const pool = await getPool();
+      const request = await pool.request();
+
+      const {
+        Code,
+        FirstName,
+        LastName,
+        School,
+        Gender,
+        DOB,
+        Email,
+        Ethnicity,
+      } = req.body;
+
+      if (!Code) {
+        return res.send({ code: 1, message: "Code Error" });
+      }
+
+      if (!FirstName || !LastName) {
+        return res.send({ code: 1, message: "Please select" });
+      }
+
+      request.input("Code", sql.VarChar, Code);
+      request.input("FirstName", sql.VarChar, FirstName);
+      request.input("LastName", sql.VarChar, LastName);
+      request.input("School", sql.VarChar, School);
+      request.input("Gender", sql.VarChar, Gender);
+      request.input("DOB", sql.VarChar, DOB);
+      request.input("Email", sql.VarChar, Email);
+      request.input("Ethnicity", sql.VarChar, Ethnicity);
+
+      const query = `
+    DECLARE @CreateDate DATETIME = GETDATE();
+    INSERT INTO tblWorkshopResult (Code,FirstName,LastName,School,Gender,DOB,Email,Ethnicity,CreateDate,isAdd)
+    VALUES (@Code,@FirstName,@LastName,@School,@Gender,@DOB,@Email,@Ethnicity,@CreateDate,1);
+    `;
+
+      request.query(query, (err) => {
+        if (err) console.log(err);
+        return res.send({
+          code: 0,
+          data: "success",
+        });
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/workshop/course-with-units",
+  isAuthenticated,
+  async function (req, res, next) {
+    try {
+      const pool = await getPool();
+      const request = await pool.request();
+      const Code = req.query.Code;
+
+      if (!Code) {
+        return res.send({ code: 400, message: "Missing workshop code" });
+      }
+
+      // Fetch both course and unit information in a single SQL query
+      const query = `
+      SELECT c.CourseID, c.CourseName, us.USName, us.UnitStandardID
+      FROM tblWorkshop w
+      JOIN tblCourse c ON w.CourseID = c.CourseID
+      LEFT JOIN tblCourseUnitStandard cus ON c.CourseID = cus.CourseID
+      LEFT JOIN tblUnitStandard us ON cus.UnitStandardID = us.UnitStandardID
+      WHERE w.Code = @Code
+    `;
+
+      request.input("Code", sql.VarChar, Code);
+
+      request.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.send({ code: 500, message: "Error fetching data" });
+        }
+
+        if (!result.recordset.length) {
+          return res.send({ code: 404, message: "No data found" });
+        }
+
+        // Get course information
+        const courseData = {
+          CourseID: result.recordset[0].CourseID,
+          CourseName: result.recordset[0].CourseName,
+        };
+
+        // Extract unit information, filtering out records without unit data
+        const units = result.recordset
+          .filter((row) => row.USName) // Filter out records without unit information
+          .map((row) => ({
+            USName: row.USName,
+            UnitStandardID: row.UnitStandardID,
+          }));
+
+        return res.send({
+          code: 0,
+          data: {
+            course: courseData,
+            units: units,
+          },
+        });
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/workshop/submit-result",
+  isAuthenticated,
+  async function (req, res, next) {
+    try {
+      const pool = await getPool();
+      const request = await pool.request();
+
+      // Extracting the parameters from the request body
+      const { WorkshopResultID, Result } = req.body;
+
+      // Check if both parameters are provided
+      if (!WorkshopResultID || !Result) {
+        return res.send({
+          code: 400,
+          message: "Missing WorkshopResultID or result",
+        });
+      }
+
+      // Convert result object to JSON string for storage in the database
+      const resultJson = JSON.stringify(Result);
+
+      // SQL query to update the Result column in tblWorkshopResult
+      const query = `
+      UPDATE tblWorkshopResult
+      SET Result = @Result
+      WHERE WorkshopResultID = @WorkshopResultID
+    `;
+
+      request.input("WorkshopResultID", sql.Int, WorkshopResultID);
+      request.input("Result", sql.VarChar(sql.MAX), resultJson);
+
+      request.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.send({ code: 500, message: "Error updating result" });
+        }
+
+        if (result.rowsAffected[0] === 0) {
+          return res.send({ code: 404, message: "WorkshopResultID not found" });
+        }
+
+        return res.send({
+          code: 0,
+          data: true,
+          message: "Result updated successfully",
+        });
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
